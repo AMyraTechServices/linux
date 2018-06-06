2611a87e1904269b2e983715b3e69d4b623e108f
grenwell <grenwell@gmail.com>
2018-06-06 13:42:30 +0000

    added new script to benchmark io

---
+++ benchmark_io.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd
+sudo mkdir /home/bench/
+cd /home/bench/
+
+echo 'CHECKING WRITE PERFORMANCE: You’ll want this to be above 400 MB/s'
+echo '================================================================='
+sudo dd if=/dev/zero of=diskbench bs=1M count=1024 conv=fdatasync
+
+
+echo 'CHECKING READ PERFORMANCE:deleting buffer cache in order to measure ‘read’ speed'
+echo '================================================================================'
+echo 3 | sudo tee /proc/sys/vm/drop_caches
+dd if=diskbench of=/dev/null bs=1M count=1024
+
+echo 'checking with buffer cache'
+echo '=========================='
+dd if=diskbench of=/dev/null bs=1M count=1024
+
+echo 'RUN THIS AT DIFFERENT TIMES TO CALCULATE AVG AND DELETE THE FILE'
+
